524baea7f3997ec0aefd066ecc89d40fdeb58bf7
ezz2003 <ezz2003@yandex.ru>
2023-12-09 06:18:50 +1000

    2 commit added constructor

--- src/Cat.java
+++ src/Cat.java
@@ -1,6 +1,10 @@
 public class Cat {
     String nameCat;
     int yearOfBirth, weight;
-
+    public Cat(String nameCat, int yearOfBirth, int weight){
+        this.nameCat = nameCat;
+        this.yearOfBirth = yearOfBirth;
+        this.weight = weight;
+    }
 
 }
