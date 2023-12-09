134340ad07d19c900b71a6268b6c44175ec43886
ezz2003 <ezz2003@yandex.ru>
2023-12-09 17:18:26 +1000

    Revert "5 commit added override equals & hashCode" by task in HW
    This reverts commit c21a658932e090113ee22ac4d722dee1e0341bf7.

--- src/Cat.java
+++ src/Cat.java
@@ -15,19 +15,6 @@ public class Cat {
     public String toString() {
         return String.format("%s %d %d ", this.nameCat, this.yearOfBirth, this.weight);
     }
-    @Override
-    public boolean equals(Object o) {
-        var c = (Cat) o;
-        return this.nameCat == c.nameCat && this.yearOfBirth == c.yearOfBirth && this.weight ==c.weight;
-    }
 
-    @Override
-    public int hashCode() {
-        int sumNumSymb = 0;
-        for (char ch : this.nameCat.toCharArray()) {
-            sumNumSymb += (int) ch;
-        }
-        return weight + sumNumSymb + this.yearOfBirth;
-    }
 
 }
